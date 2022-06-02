4dda58dc148c4e1fd20e629798170be9a83419a2
Guilherme Janczak <guilherme.janczak@yandex.com>
2022-06-02 03:08:19 +0000

    polish into a real program

---
+++ README.md
@@ -0,0 +1,16 @@
+# dl-applefonts: download and install Apple fonts
+## Usage
+This program depends on [p7zip](https://github.com/jinfeihan57/p7zip), a POSIX
+shell, and a downloader program which can be any of BSD ftp (recommended),
+[curl](https://github.com/curl/curl), and
+[wget](https://www.gnu.org/software/wget/).
+
+Simply run the command:
+```console
+foo@bar$ sh dl-applefonts.sh
+```
+Apple's fonts will be installed to _~/.local/share/fonts_ by default. The `-o`
+option allows specifying a custom directory.
+
+This program has been tested on OpenBSD, but is expected to run on other
+systems.
---
+++ dl-applefonts.sh
@@ -0,0 +1,152 @@
+#!/bin/sh -e
+
+# Copyright (c) 2022 Guilherme Janczak <guilherme.janczak@yandex.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+readonly progname="${0##*/}"
+outdir="$HOME/.local/share/fonts"
+
+# This script downloads Apple's system fonts from Apple's website and installs
+# them.
+main()
+{
+	if ! command -v 7z >/dev/null; then
+		err "p7zip dependency missing"
+	fi
+
+	while getopts ho: o; do
+		case $o in
+			h)	usage; rval=0;;
+			o)	outdir="$OPTARG";;
+			?)	exit 1;;
+		esac
+	done
+	shift $((OPTIND - 1))
+	if [ "$rval" ]; then
+		exit "$rval"
+	fi
+	if [ "$*" ]; then
+		err "extraneous non-option arguments: '$*'"
+	fi
+
+	trap err_handler EXIT
+	infile="$(mktemp)"
+	outfile="$(mktemp)"
+
+	getfont 'SF Pro'
+	getfont 'SF Compact'
+	getfont 'SF Mono'
+	getfont 'SF Arabic'
+	getfont 'NY'
+}
+
+# err: print an error message and error exit
+err()
+{
+	printf '%s: %s\n' "$progname" "$1"
+	exit 1
+}
+
+usage()
+{
+	cat <<- EOF
+		usage:	$progname [-h] [-o outdir]
+	EOF
+}
+
+# err_handler: remove temporary files on error.
+# XXX: figure out a clean way to also remove the partial installed files
+err_handler()
+{
+	rm "$infile" "$outfile"
+}
+
+# getfont: download and install Apple font
+#
+# $1: one of {SF Pro, SF Compact, SF Mono, SF Arabic, NY}.
+#
+# global variable $outdir: output directory (created by the function if needed).
+# global variables $infile $outfile: temporary files.
+getfont()
+{
+	font="$1"
+
+	# The filename of the font's .dmg in Apple's website is the name of the
+	# font but with the space if any replaced by a dash.
+	dl="$(printf '%s' "$font" | sed 's/ /-/')"
+	url="https://devimages-cdn.apple.com/design/resources/download/$dl.dmg"
+	download "$infile" "$url"
+
+	# The .dmg file is full of random stuff we don't care about. What we do
+	# care about is the .pkg file in it which contains the payload.
+	#
+	# Look at how the font's name becomes the .pkg's name:
+	# font: SF Pro
+	# .pkg: SFProFonts/SF Pro Fonts.pkg
+	# font: NY
+	# .pkg: NYFonts/NY Fonts.pkg
+	#
+	# The text processing here transforms the font name into the .pkg name.
+	filt="$(printf '%s' "$font" | sed 's/ //')"
+	filt="${filt}Fonts/$font Fonts.pkg"
+	7z -so e "$infile" "$filt" > "$outfile"
+	swap
+
+	# Don't need to specify a filter here but I do anyway for strictness.
+	7z -so e "$infile" 'Payload~' > "$outfile"
+	swap
+
+	# 7z creates the outdir if necessary.
+	# The fonts are inside ./Library/Fonts/ in the payload. .ttf and .otf
+	# fonts are in there, save for Arabic which only has .ttf, but we only
+	# care about .otf.
+	if [ "$font" = "SF Arabic" ]; then
+		ext=ttf
+	else
+		ext=otf
+	fi
+	7z -o"$outdir/AppleFonts/$font" e "$infile" "./Library/Fonts/*.$ext" \
+		>/dev/null
+}
+
+# download(): download $2 to $1
+if command -v ftp >/dev/null; then
+	download()
+	{
+		ftp -o "$1" "$2"
+	}
+elif command -v curl >/dev/null; then
+	download()
+	{
+		curl -o "$1" "$2"
+	}
+elif command -v wget >/dev/null; then
+	download()
+	{
+		wget -O "$1" "$2"
+	}
+else
+	err "downloader dependency missing, install any of: ftp, curl, wget"
+fi
+
+# swap(): swap global variables $infile and $outfile
+swap()
+{
+	swap="$infile"
+	infile="$outfile"
+	outfile="$swap"
+	unset swap
+}
+
+main "$@"
